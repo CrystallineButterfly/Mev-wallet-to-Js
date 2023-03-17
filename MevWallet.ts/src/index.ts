21cfd4acef9e0b40861e036c7464c86c7cdf8b35
Kell (K42) <fromtc2k42@gmail.com>
2023-03-17 02:38:33 +0000

    Update index.ts to Js

--- MevWallet.ts/src/index.ts
+++ MevWallet.ts/src/index.ts
@@ -1,28 +1,28 @@
-import * as ethers from 'ethers';
+const ethers = require('ethers');
 
-export { MevTxBuilder } from './builder';
-export type {
-  MevWalletV1Abi as MevWalletV1,
-  MevWethAbi as MevWeth,
-  MevWalletFactoryV1Abi as MevWalletFactoryV1,
-} from './bindings';
-export {
-  MevWalletV1Abi__factory as MevWalletV1__factory,
-  MevWethAbi__factory as MevWeth__factory,
-  MevWalletFactoryV1Abi__factory as MevWalletFactory__factory,
-} from './bindings';
+module.exports.MevTxBuilder = require('./builder').MevTxBuilder;
 
-export { getFactory } from './deploy';
+const {
+  MevWalletV1Abi: MevWalletV1,
+  MevWethAbi: MevWeth,
+  MevWalletFactoryV1Abi: MevWalletFactoryV1,
+} = require('./bindings');
 
-export * as utils from './utils';
+const {
+  MevWalletV1Abi__factory: MevWalletV1__factory,
+  MevWethAbi__factory: MevWeth__factory,
+  MevWalletFactoryV1Abi__factory: MevWalletFactory__factory,
+} = require('./bindings');
 
-export const MEV_WETH_ADDRESS = '0x00000000008C43efC014746c230049e330039Cb3';
-export const MEV_WALLET_IMPL_ADDRESS =
-  '0x00000000004096437C84E1B0927D5ED44F45F6b3';
-export const MEV_WALLET_FACTORY_ADDRESS =
-  '0x49496DD21760ED9235aFE43871983869CC0eFC61';
+module.exports.getFactory = require('./deploy').getFactory;
 
-export const MEV_TX_TYPES: Record<string, Array<ethers.TypedDataField>> = {
+module.exports.utils = require('./utils');
+
+module.exports.MEV_WETH_ADDRESS = '0x00000000008C43efC014746c230049e330039Cb3';
+module.exports.MEV_WALLET_IMPL_ADDRESS = '0x00000000004096437C84E1B0927D5ED44F45F6b3';
+module.exports.MEV_WALLET_FACTORY_ADDRESS = '0x49496DD21760ED9235aFE43871983869CC0eFC61';
+
+module.exports.MEV_TX_TYPES = {
   MevTx: [
     { name: 'to', type: 'address' },
     { name: 'data', type: 'bytes' },
@@ -35,26 +35,39 @@ export const MEV_TX_TYPES: Record<string, Array<ethers.TypedDataField>> = {
   ],
 };
 
-export type MevTx = {
-  to: string;
-  data: string;
-  value: ethers.BigNumber;
-  delegate: boolean;
-  tip: ethers.BigNumber;
-  maxBaseFee: ethers.BigNumber;
-  timing: ethers.BigNumber;
-  nonce: ethers.BigNumber;
+module.exports.MevTx = {
+  to: '',
+  data: '',
+  value: ethers.BigNumber.from(0),
+  delegate: false,
+  tip: ethers.BigNumber.from(0),
+  maxBaseFee: ethers.BigNumber.from(0),
+  timing: ethers.BigNumber.from(0),
+  nonce: ethers.BigNumber.from(0),
 };
 
-export type SerializedMevTx = {
-  to: string;
-  data: string;
-  value: string;
-  delegate: boolean;
-  tip: string;
-  maxBaseFee: string;
-  timing: string;
-  nonce: string;
+module.exports.SerializedMevTx = {
+  to: '',
+  data: '',
+  value: '',
+  delegate: false,
+  tip: '',
+  maxBaseFee: '',
+  timing: '',
+  nonce: '',
 };
 
-export type SignedMevTx = SerializedMevTx & { v: number; r: string; s: string };
+module.exports.SignedMevTx = {
+  to: '',
+  data: '',
+  value: '',
+  delegate: false,
+  tip: '',
+  maxBaseFee: '',
+  timing: '',
+  nonce: '',
+  v: 0,
+  r: '',
+  s: '',
+};
+
